a82590a70931326cc03f46f3cedeedf2895da3a3
marviorocha <marviorocha@marviorocha.com>
2021-05-28 11:21:20 -0300

    Add FontAwesome 5 to Project

--- awesomeapp/app/javascript/packs/application.js
+++ awesomeapp/app/javascript/packs/application.js
@@ -2,5 +2,13 @@ import Rails from "@rails/ujs"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "stylesheets/application"
+
+// fontawesome 5
+
+import '@fortawesome/fontawesome-free/js/fontawesome'
+import '@fortawesome/fontawesome-free/js/solid'
+import '@fortawesome/fontawesome-free/js/regular'
+import '@fortawesome/fontawesome-free/js/brands'
+
 Rails.start()
 ActiveStorage.start()
